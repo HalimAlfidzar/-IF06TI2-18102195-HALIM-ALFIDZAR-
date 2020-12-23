8b6b0d0e053d4bfaa6e1a0d5f158e71f432bec02
Halim Alfidzar <52267549+HalimAlfidzar@users.noreply.github.com>
2020-12-23 16:12:55 +0700

    Create simpan data pada shared preference dan menampilkan data shared preference pada MainActivity.kt

--- praktikum9/src/main/java/com/halim_18102195/praktikum9/MainActivity.kt
+++ praktikum9/src/main/java/com/halim_18102195/praktikum9/MainActivity.kt
@@ -2,14 +2,36 @@ package com.halim_18102195.praktikum9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.halim_18102195.praktikum9.data.SettingModel
 import com.halim_18102195.praktikum9.databinding.ActivityMainBinding
+import com.halim_18102195.praktikum9.preference.SettingPreference
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var settingModel: SettingModel
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mSettingPreference: SettingPreference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.act = this
         setContentView(binding.root)
         supportActionBar?.title = getString(R.string.main_title)
+        mSettingPreference = SettingPreference(this)
+        showExistingPreference()
+    }
+    private fun showExistingPreference() {
+        settingModel = mSettingPreference.getSetting()
+        populateView(settingModel)
+    }
+    private fun populateView(settingModel: SettingModel) {
+        if (settingModel.isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            delegate.applyDayNight()
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            delegate.applyDayNight()
+        }
+        binding.settingModel = settingModel
     }
 }
