395beeecef03a2af88743a62ba498d51a942fb1f
Halim Alfidzar <52267549+HalimAlfidzar@users.noreply.github.com>
2021-01-03 20:27:58 +0700

    Init Praktikum 10

---
+++ praktikum10/src/main/java/com/halim_18102195/praktikum10/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.halim_18102195.praktikum10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
