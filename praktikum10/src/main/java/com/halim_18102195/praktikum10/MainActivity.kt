fcc079cc80f893629978b606456739520c46c2f3
Halim Alfidzar <52267549+HalimAlfidzar@users.noreply.github.com>
2021-01-05 20:16:36 +0700

    Add kode pada onCreate pada MainActivity.kt

--- praktikum10/src/main/java/com/halim_18102195/praktikum10/MainActivity.kt
+++ praktikum10/src/main/java/com/halim_18102195/praktikum10/MainActivity.kt
@@ -2,7 +2,9 @@ package com.halim_18102195.praktikum10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.halim_18102195.praktikum10.adapter.QuoteAdapter
+import com.halim_18102195.praktikum10.data.Quote
 import com.halim_18102195.praktikum10.databinding.ActivityMainBinding
 import com.halim_18102195.praktikum10.db.QuoteHelper
 
@@ -14,6 +16,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Quotes"
+        binding.rvQuotes.layoutManager = LinearLayoutManager(this)
+        binding.rvQuotes.setHasFixedSize(true)
+        adapter = QuoteAdapter(this)
+        binding.rvQuotes.adapter = adapter
+
+        quoteHelper = QuoteHelper.getInstance(applicationContext)
+        quoteHelper.open()
+        if (savedInstanceState == null) {
+            loadQuotes()
+        } else {
+            val list = savedInstanceState.getParcelableArrayList<Quote>(EXTRA_STATE)
+            if (list != null) {
+                adapter.listQuotes = list
+            }
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList(EXTRA_STATE, adapter.listQuotes)
     }
 }
